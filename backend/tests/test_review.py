6eaa1cc34d11d3449840ae95622cceae5116af0a
hackymatt <106622396+hackymatt@users.noreply.github.com>
2023-10-18 14:36:25 +0200

    Add best reviews endpoint

--- backend/tests/test_review.py
+++ backend/tests/test_review.py
@@ -139,16 +139,16 @@ class ReviewTest(APITestCase):
             lesson=self.course.lessons.all()[1], profile=self.profile_3, rating=5
         )
 
-    def test_get_best_reviews_unauthenticated(self):
+    def test_get_reviews_unauthenticated(self):
         # no login
         self.assertFalse(auth.get_user(self.client).is_authenticated)
         # get data
         response = self.client.get(self.endpoint)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = json.loads(response.content)
-        self.assertEqual(len(data), 2)
+        self.assertEqual(len(data), 6)
 
-    def test_get_best_reviews_authenticated(self):
+    def test_get_reviews_authenticated(self):
         # login
         login(self, self.data["email"], self.data["password"])
         self.assertTrue(auth.get_user(self.client).is_authenticated)
@@ -156,7 +156,26 @@ class ReviewTest(APITestCase):
         response = self.client.get(self.endpoint)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = json.loads(response.content)
-        self.assertEqual(len(data), 2)
+        self.assertEqual(len(data), 6)
+
+    def test_get_course_reviews_unauthenticated(self):
+        # no login
+        self.assertFalse(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(f"{self.endpoint}?course_id={self.course.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        self.assertEqual(len(data), 6)
+
+    def test_get_course_reviews_authenticated(self):
+        # login
+        login(self, self.data["email"], self.data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(f"{self.endpoint}?course_id={self.course.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        self.assertEqual(len(data), 6)
 
     def test_create_review_unauthenticated(self):
         # no login
@@ -279,3 +298,140 @@ class ReviewTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(reviews_number(), 5)
         self.assertFalse(is_review_found(self.review_1.id))
+
+
+class BestReviewTest(APITestCase):
+    def setUp(self):
+        self.endpoint = "/best-reviews"
+        self.data = {
+            "email": "user@example.com",
+            "password": "TestPassword123",
+        }
+        self.user_1 = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.data["email"],
+            password=self.data["password"],
+            is_active=True,
+        )
+        self.user_2 = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email="user2@example.com",
+            password="TestPassword123",
+            is_active=True,
+        )
+        self.user_3 = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email="user3@example.com",
+            password="TestPassword123",
+            is_active=True,
+        )
+        self.profile_1 = create_profile(user=self.user_1)
+        self.profile_2 = create_profile(user=self.user_2)
+        self.profile_3 = create_profile(user=self.user_3)
+
+        self.lecturer_user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email="lecturer_1@example.com",
+            password=self.data["password"],
+            is_active=True,
+        )
+        self.lecturer_profile = create_profile(user=self.lecturer_user, user_type="W")
+
+        self.course = create_course(
+            title="course_title",
+            description="course_description",
+            technology=create_technology_obj(name="Python"),
+            level="Podstawowy",
+            price="99.99",
+            github_repo_link="www.example.com",
+            skills=[create_skill_obj(name="coding"), create_skill_obj(name="IDE")],
+            topics=[
+                create_topic_obj(name="You will learn how to code"),
+                create_topic_obj(name="You will learn a new IDE"),
+            ],
+            lessons=[
+                create_lesson_obj(
+                    id=-1,
+                    title="Python lesson 1",
+                    description="bbbb",
+                    duration="90",
+                    github_branch_link="https://github.com/hackymatt/CodeEdu",
+                    price="9.99",
+                    lecturers=[
+                        create_lecturer_obj(self.lecturer_profile),
+                    ],
+                ),
+                create_lesson_obj(
+                    id=-1,
+                    title="Python lesson 2",
+                    description="bbbb",
+                    duration="30",
+                    github_branch_link="https://github.com/hackymatt/CodeEdu",
+                    price="2.99",
+                    lecturers=[create_lecturer_obj(self.lecturer_profile)],
+                ),
+                create_lesson_obj(
+                    id=-1,
+                    title="Python lesson 3",
+                    description="bbbb",
+                    duration="30",
+                    github_branch_link="https://github.com/hackymatt/CodeEdu",
+                    price="2.99",
+                    lecturers=[create_lecturer_obj(self.lecturer_profile)],
+                ),
+            ],
+        )
+
+        self.review_1 = create_review(
+            lesson=self.course.lessons.all()[0],
+            profile=self.profile_1,
+            rating=5,
+            review="Great lesson.",
+        )
+        self.review_2 = create_review(
+            lesson=self.course.lessons.all()[0],
+            profile=self.profile_2,
+            rating=5,
+            review="Super helpful.",
+        )
+        self.review_3 = create_review(
+            lesson=self.course.lessons.all()[0],
+            profile=self.profile_3,
+            rating=4,
+            review="Great lesson.",
+        )
+        self.review_4 = create_review(
+            lesson=self.course.lessons.all()[1], profile=self.profile_1, rating=3
+        )
+        self.review_5 = create_review(
+            lesson=self.course.lessons.all()[1],
+            profile=self.profile_2,
+            rating=2,
+            review="Terrible.",
+        )
+        self.review_6 = create_review(
+            lesson=self.course.lessons.all()[1], profile=self.profile_3, rating=5
+        )
+
+    def test_get_best_reviews_unauthenticated(self):
+        # no login
+        self.assertFalse(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        self.assertEqual(len(data), 2)
+
+    def test_get_best_reviews_authenticated(self):
+        # login
+        login(self, self.data["email"], self.data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        self.assertEqual(len(data), 2)
