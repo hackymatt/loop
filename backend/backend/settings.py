43c2b9ecbb3b2b48d3c8a0d65bce22cb3fe5fe18
hackymatt <106622396+hackymatt@users.noreply.github.com>
2023-11-22 16:23:41 +0100

    Add cache mechanism

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
         "LOCATION": "cache:11211",
     }
 }
