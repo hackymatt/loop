b43d1c1606b9b8e51040796295333df7c617c739
hackymatt <106622396+hackymatt@users.noreply.github.com>
2023-11-24 22:17:01 +0100

    Add cache mechanism

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,7 +65,7 @@ MIDDLEWARE = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://cache:6379/0",
+        "LOCATION": "redis://127.0.0.1:6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
