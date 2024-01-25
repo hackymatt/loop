702bfd4b14da8f582ca13334d55570c5167ebc80
hackymatt <106622396+hackymatt@users.noreply.github.com>
2024-01-25 11:28:07 +0100

    Add submit query

--- frontend/src/sections/_elearning/contact/contact-form.tsx
+++ frontend/src/sections/_elearning/contact/contact-form.tsx
@@ -11,6 +11,8 @@ import LoadingButton from "@mui/lab/LoadingButton";
 
 import { useResponsive } from "src/hooks/use-responsive";
 
+import { useContact } from "src/api/contact/contact";
+
 import Image from "src/components/image";
 import FormProvider, { RHFTextField } from "src/components/hook-form";
 
@@ -19,15 +21,17 @@ import FormProvider, { RHFTextField } from "src/components/hook-form";
 export default function ContactForm() {
   const mdUp = useResponsive("up", "md");
 
+  const { mutateAsync: contact } = useContact();
+
   const ContactSchema = Yup.object().shape({
-    fullName: Yup.string().required("Imię i Nazwisko jest wymagane"),
+    full_name: Yup.string().required("Imię i Nazwisko jest wymagane"),
     email: Yup.string().required("Adres email jest wymagany").email("Podaj poprawny adres e-mail"),
     subject: Yup.string().required("Temat jest wymagany"),
     message: Yup.string().required("Wiadomość jest wymagana"),
   });
 
   const defaultValues = {
-    fullName: "",
+    full_name: "",
     subject: "",
     email: "",
     message: "",
@@ -46,9 +50,8 @@ export default function ContactForm() {
 
   const onSubmit = handleSubmit(async (data) => {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 500));
+      await contact(data);
       reset();
-      console.log("DATA", data);
     } catch (error) {
       console.error(error);
     }
@@ -90,7 +93,7 @@ export default function ContactForm() {
 
             <FormProvider methods={methods} onSubmit={onSubmit}>
               <Stack spacing={2.5} alignItems="flex-start">
-                <RHFTextField name="fullName" label="Imię i nazwisko" />
+                <RHFTextField name="full_name" label="Imię i nazwisko" />
 
                 <RHFTextField name="email" label="Adres e-mail" />
 
