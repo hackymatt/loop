2fe4deef5a448adce2f33cae2245687ad98d9613
hackymatt <106622396+hackymatt@users.noreply.github.com>
2024-01-28 19:01:09 +0100

    Change NotFound view

--- frontend/src/sections/error/not-found-view.tsx
+++ frontend/src/sections/error/not-found-view.tsx
@@ -7,6 +7,7 @@ import Typography from "@mui/material/Typography";
 
 import { RouterLink } from "src/routes/components";
 
+import MainLayout from "src/layouts/main";
 import CompactLayout from "src/layouts/compact";
 
 import Image from "src/components/image";
@@ -16,37 +17,39 @@ import { varBounce, MotionContainer } from "src/components/animate";
 
 export default function NotFoundView() {
   return (
-    <CompactLayout>
-      <MotionContainer>
-        <m.div variants={varBounce().in}>
-          <Typography variant="h3" paragraph>
-            Strona nie istnieje!
-          </Typography>
-        </m.div>
+    <MainLayout disabledHeader disabledFooter>
+      <CompactLayout>
+        <MotionContainer>
+          <m.div variants={varBounce().in}>
+            <Typography variant="h3" paragraph>
+              Strona nie istnieje!
+            </Typography>
+          </m.div>
 
-        <m.div variants={varBounce().in}>
-          <Typography sx={{ color: "text.secondary" }}>
-            Przepraszamy, nie znaleźliśmy strony, której szukasz. Być może błędnie wpisałeś adres
-            URL? Pamiętaj, aby sprawdzić pisownię.
-          </Typography>
-        </m.div>
+          <m.div variants={varBounce().in}>
+            <Typography sx={{ color: "text.secondary" }}>
+              Przepraszamy, nie znaleźliśmy strony, której szukasz. Być może błędnie wpisałeś adres
+              URL? Pamiętaj, aby sprawdzić pisownię.
+            </Typography>
+          </m.div>
 
-        <m.div variants={varBounce().in}>
-          <Image
-            alt="404"
-            src="/assets/illustrations/illustration_404.svg"
-            sx={{
-              mx: "auto",
-              maxWidth: 320,
-              my: { xs: 5, sm: 8 },
-            }}
-          />
-        </m.div>
+          <m.div variants={varBounce().in}>
+            <Image
+              alt="404"
+              src="/assets/illustrations/illustration_404.svg"
+              sx={{
+                mx: "auto",
+                maxWidth: 320,
+                my: { xs: 5, sm: 8 },
+              }}
+            />
+          </m.div>
 
-        <Button component={RouterLink} href="/" size="large" color="inherit" variant="contained">
-          Wróć do strony głownej
-        </Button>
-      </MotionContainer>
-    </CompactLayout>
+          <Button component={RouterLink} href="/" size="large" color="inherit" variant="contained">
+            Wróć do strony głownej
+          </Button>
+        </MotionContainer>
+      </CompactLayout>
+    </MainLayout>
   );
 }
