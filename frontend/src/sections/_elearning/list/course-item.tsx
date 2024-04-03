4da415614a74f4fe44fb0709895d8cd6f9e747fc
hackymatt <106622396+hackymatt@users.noreply.github.com>
2024-04-03 15:05:11 +0200

    Change hour display

--- frontend/src/sections/_elearning/list/course-item.tsx
+++ frontend/src/sections/_elearning/list/course-item.tsx
@@ -238,7 +238,7 @@ export default function CourseItem({ course, vertical }: Props) {
         >
           <Stack direction="row" alignItems="center" sx={{ typography: "body2" }}>
             <Iconify icon="carbon:time" sx={{ mr: 1 }} />
-            {totalHours < 1 ? 0 : fShortenNumber(Math.floor(totalHours), 0)}+{" "}
+            {totalHours < 1 ? totalHours : fShortenNumber(Math.floor(totalHours), 0)}+{" "}
             {polishPlurals("godzina", "godziny", "godzin", totalHours)}
           </Stack>
 
