2442fea3ab6195275a36819eeea212e4c842a624
hackymatt <106622396+hackymatt@users.noreply.github.com>
2024-04-15 20:01:07 +0200

    Change models

--- backend/coupon/admin.py
+++ backend/coupon/admin.py
@@ -1,20 +1,5 @@
 from django.contrib import admin
-from .models import (
-    Coupon,
-    CouponUser,
-    Rules,
-    MaxUsageRule,
-    AllowedUsersRule,
-    Discount,
-    MinPurchaseTotalRule,
-    ValidityRule,
-)
+from .models import Coupon, CouponUser
 
 admin.site.register(Coupon)
 admin.site.register(CouponUser)
-admin.site.register(Rules)
-admin.site.register(MaxUsageRule)
-admin.site.register(AllowedUsersRule)
-admin.site.register(Discount)
-admin.site.register(MinPurchaseTotalRule)
-admin.site.register(ValidityRule)
--- backend/coupon/models.py
+++ backend/coupon/models.py
@@ -17,35 +17,20 @@ from decimal import Decimal
 from profile.models import Profile
 
 
-class AllowedUsersRule(BaseModel):
+class Coupon(BaseModel):
+    code = CharField(
+        max_length=36,
+        unique=True,
+    )
+    discount = IntegerField(default=0)
+    is_percentage = BooleanField(default=False)
     users = ManyToManyField(Profile, blank=True)
     all_users = BooleanField(default=False)
-
-    class Meta:
-        db_table = "coupon_allowed_users_rule"
-        ordering = ["id"]
-
-
-class MaxUsageRule(BaseModel):
     max_uses = BigIntegerField(default=0)
     is_infinite = BooleanField(default=False)
     uses_per_user = IntegerField(default=1)
-
-    class Meta:
-        db_table = "coupon_usage_rule"
-        ordering = ["id"]
-
-
-class ValidityRule(BaseModel):
     expiration_date = DateTimeField()
     is_active = BooleanField(default=False)
-
-    class Meta:
-        db_table = "coupon_validity_rule"
-        ordering = ["id"]
-
-
-class MinPurchaseTotalRule(BaseModel):
     min_total = DecimalField(
         max_digits=7,
         decimal_places=2,
@@ -53,46 +38,10 @@ class MinPurchaseTotalRule(BaseModel):
         default=0,
     )
 
-    class Meta:
-        db_table = "coupon_min_purchase_total_rule"
-        ordering = ["id"]
-
-
-class Rules(BaseModel):
-    allowed_users = ForeignKey(AllowedUsersRule, on_delete=CASCADE)
-    max_usage = ForeignKey(MaxUsageRule, on_delete=CASCADE)
-    validity = ForeignKey(ValidityRule, on_delete=CASCADE)
-    min_purchase_total = ForeignKey(MinPurchaseTotalRule, on_delete=CASCADE)
-
-    class Meta:
-        db_table = "coupon_rules"
-        ordering = ["id"]
-
-
-class Discount(BaseModel):
-    value = IntegerField(default=0)
-    is_percentage = BooleanField(default=False)
-
-    class Meta:
-        db_table = "coupon_discount"
-        ordering = ["id"]
-
-
-class Coupon(BaseModel):
-    code = CharField(
-        max_length=36,
-        unique=True,
-    )
-    discount = ForeignKey(Discount, on_delete=CASCADE)
-    times_used = IntegerField(default=0)
-
-    rules = ForeignKey(Rules, on_delete=CASCADE)
-
 
 class CouponUser(BaseModel):
     user = ForeignKey(Profile, on_delete=CASCADE)
     coupon = ForeignKey(Coupon, on_delete=CASCADE)
-    times_used = IntegerField(default=0)
 
     class Meta:
         db_table = "coupon_user"
