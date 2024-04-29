9f3f9d42ed61e00db1db2ca74659c77fc96b6da5
hackymatt <106622396+hackymatt@users.noreply.github.com>
2024-04-29 19:56:03 +0200

    Added backup

--- backend/main.sh
+++ backend/main.sh
@@ -11,6 +11,13 @@ then
     echo "PostgreSQL started"
 fi
 
+touch /var/log/cron.log
+printenv | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID|LANG|PWD|GPG_KEY|_=' >> /etc/environment
+
+python manage.py crontab remove
+python manage.py crontab add
+service cron start
+
 python manage.py migrate
 
 exec "$@"
