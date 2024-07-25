715718cb3a1e7189fd5cea1d6a1f358a097d5913
hackymatt <106622396+hackymatt@users.noreply.github.com>
2024-07-25 09:57:50 +0200

    Fix linting

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -253,7 +253,7 @@ else:
                 "location": f"{ENV}/static" if ENV != "PROD" else "static",
             },
         },
-    } # pragma: no cover
+    }  # pragma: no cover
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
