2014a6f19b750bf1e66a1f1a9fcad57271a1d209
hackymatt <106622396+hackymatt@users.noreply.github.com>
2024-10-01 12:15:14 +0200

    Add suspense

--- frontend/src/utils/google-analytics.ts
+++ frontend/src/utils/google-analytics.ts
@@ -6,15 +6,19 @@ export const updateConsent = (consentValues: IConsentValuesProp) =>
   window.gtag("consent", "update", consentValues);
 
 export const pageView = (measurementId: string, url: string) => {
-  window.gtag("config", measurementId, {
-    page_path: url,
-  });
+  if (window.gtag) {
+    window.gtag("config", measurementId, {
+      page_path: url,
+    });
+  }
 };
 
 export const trackEvent = (action: string, category: string, label: string, value: string) => {
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value,
-  });
+  if (window.gtag) {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value,
+    });
+  }
 };
