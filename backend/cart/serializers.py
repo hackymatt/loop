6952082c61facae418c4946b3378aa620d286b17
loopedupl <admin@loop.edu.pl>
2024-10-15 22:35:20 +0200

    Amend cart

--- backend/cart/serializers.py
+++ backend/cart/serializers.py
@@ -1,58 +1,43 @@
-from rest_framework.serializers import (
-    ModelSerializer,
-    SerializerMethodField,
-)
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from lesson.models import Lesson, Technology
 from cart.models import Cart
-from profile.models import Profile, LecturerProfile, StudentProfile
-from teaching.models import Teaching
+from profile.models import LecturerProfile, StudentProfile
+from django.db.models import Value
 from django.db.models.functions import Concat
-from django.db.models import Value, Q
 
 
 class LecturerSerializer(ModelSerializer):
-    full_name = SerializerMethodField("get_full_name")
+    full_name = SerializerMethodField()
 
     class Meta:
         model = LecturerProfile
         fields = ("full_name",)
 
     def get_full_name(self, lecturer):
-        return lecturer.profile.user.first_name + " " + lecturer.profile.user.last_name
+        return lecturer.full_name
 
 
 class TechnologySerializer(ModelSerializer):
     class Meta:
         model = Technology
-        exclude = (
-            "id",
-            "modified_at",
-            "created_at",
-        )
+        exclude = ("id", "modified_at", "created_at")
 
 
 class LessonSerializer(ModelSerializer):
     technologies = TechnologySerializer(many=True)
-    lecturers = SerializerMethodField("get_lesson_lecturers")
+    lecturers = SerializerMethodField()
 
     class Meta:
         model = Lesson
-        fields = (
-            "id",
-            "title",
-            "duration",
-            "price",
-            "technologies",
-            "lecturers",
-        )
+        fields = ("id", "title", "duration", "price", "technologies", "lecturers")
 
-    def get_lesson_lecturers(self, lesson):
-        lecturer_ids = Teaching.objects.filter(lesson=lesson).values("lecturer")
+    def get_lecturers(self, lesson):
         lecturers = (
-            LecturerProfile.objects.exclude(
-                Q(title__isnull=True) | Q(description__isnull=True)
+            LecturerProfile.objects.filter(
+                teaching_lecturer__lesson=lesson,  # Use the correct related name here
+                title__isnull=False,
+                description__isnull=False,
             )
-            .filter(id__in=lecturer_ids)
             .annotate(
                 full_name=Concat(
                     "profile__user__first_name", Value(" "), "profile__user__last_name"
@@ -80,10 +65,6 @@ class CartSerializer(ModelSerializer):
 
     def create(self, validated_data):
         user = self.context["request"].user
-        student = Profile.objects.get(user=user)
-
-        obj, _ = Cart.objects.get_or_create(
-            student=StudentProfile.objects.get(profile=student), **validated_data
-        )
-
+        student = StudentProfile.objects.get(profile__user=user)
+        obj, _ = Cart.objects.get_or_create(student=student, **validated_data)
         return obj
