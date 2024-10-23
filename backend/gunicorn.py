5923546376542bffeeb5b4acfc269deda236fd4d
loopedupl <admin@loop.edu.pl>
2024-10-23 13:12:49 +0200

    Amend gunicorn

--- backend/gunicorn.py
+++ backend/gunicorn.py
@@ -3,13 +3,10 @@ import multiprocessing
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "core.wsgi"
 # The number of worker processes for handling requests
-workers = (multiprocessing.cpu_count() * 3) + 1
+workers = multiprocessing.cpu_count() * 2 + 1
 # Timeout
-timeout = 30
+timeout = 60
 # The socket to bind
 bind = "0.0.0.0:8000"
 # Daemonize the Gunicorn process (detach & enter background)
 daemon = True
-# Access log configuration (optional)
-accesslog = "-"  # Log to stdout, useful for Docker
-errorlog = "-"  # Log errors to stdout
