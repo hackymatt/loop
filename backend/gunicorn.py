d8a17ac02cca6bd910228daf4acc2717aa2e1345
loopedupl <admin@loop.edu.pl>
2024-10-23 12:58:55 +0200

    Amend gunicorn

--- backend/gunicorn.py
+++ backend/gunicorn.py
@@ -1,11 +1,4 @@
 import multiprocessing
-import os
-from dotenv import load_dotenv
-
-
-load_dotenv()
-
-LOCAL = os.getenv("LOCAL", "True") == "True"
 
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "core.wsgi"
@@ -16,7 +9,7 @@ timeout = 30
 # The socket to bind
 bind = "0.0.0.0:8000"
 # Daemonize the Gunicorn process (detach & enter background)
-daemon = not LOCAL
+daemon = True
 # Access log configuration (optional)
 accesslog = "-"  # Log to stdout, useful for Docker
 errorlog = "-"  # Log errors to stdout
