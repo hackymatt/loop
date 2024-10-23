5c2f3f460cf77ef0516f2b86b19add6f6608bd5e
loopedupl <admin@loop.edu.pl>
2024-10-23 14:54:45 +0200

    Amend gunicorn

--- backend/gunicorn.py
+++ backend/gunicorn.py
@@ -3,7 +3,7 @@ import multiprocessing
 # Django WSGI application path in pattern MODULE_NAME:VARIABLE_NAME
 wsgi_app = "core.wsgi"
 # The number of worker processes for handling requests
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = multiprocessing.cpu_count() * 3 + 1
 # Timeout
 timeout = 30
 # The socket to bind
