cefa8e8b33d6b4487cde4b50a0bbb029661e3c96
loopedupl <admin@loop.edu.pl>
2024-10-26 19:36:41 +0200

    Add page title

--- frontend/src/app/course/[id]/page.tsx
+++ frontend/src/app/course/[id]/page.tsx
@@ -1,4 +1,8 @@
+import { Metadata } from "next";
+
 import { ViewUtil } from "src/utils/page-utils";
+import { decodeUrl } from "src/utils/url-utils";
+import { createMetadata } from "src/utils/create-metadata";
 
 import CourseView from "src/sections/view/course-view";
 
@@ -6,4 +10,26 @@ import CourseView from "src/sections/view/course-view";
 
 export default function CoursePage({ params }: { params: { id: string } }) {
   return <ViewUtil defaultView={<CourseView id={params.id} />} />;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const decodedId = decodeUrl(params.id);
+  const courseTitle = decodedId.slice(0, decodedId.lastIndexOf("-") - 1);
+
+  const metadata = createMetadata(
+    `Kurs: ${courseTitle}`,
+    `Zapisz się na kurs ${courseTitle} w loop i naucz się programować. Oferujemy praktyczne lekcje online z certyfikatem ukończenia oraz wsparcie doświadczonych instruktorów.`,
+    [
+      `kurs ${courseTitle}`,
+      "kursy programowania",
+      "szkoła programowania loop",
+      "kurs programowania",
+    ],
+  );
+
+  return {
+    title: metadata.title,
+    description: metadata.description,
+    keywords: metadata.keywords,
+  };
 }
--- frontend/src/app/teacher/[id]/page.tsx
+++ frontend/src/app/teacher/[id]/page.tsx
@@ -1,4 +1,8 @@
+import { Metadata } from "next";
+
 import { ViewUtil } from "src/utils/page-utils";
+import { decodeUrl } from "src/utils/url-utils";
+import { createMetadata } from "src/utils/create-metadata";
 
 import TeacherView from "src/sections/view/teacher-view";
 
@@ -6,4 +10,32 @@ import TeacherView from "src/sections/view/teacher-view";
 
 export default function TeacherPage({ params }: { params: { id: string } }) {
   return <ViewUtil defaultView={<TeacherView id={params.id} />} />;
+}
+
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const decodedId = decodeUrl(params.id);
+  const teacherName = decodedId.slice(0, decodedId.lastIndexOf("-") - 1);
+
+  const metadata = createMetadata(
+    `Instruktor: ${teacherName}`,
+    `Poznaj ${teacherName} — doświadczonego instruktora w loop. Sprawdź jego profil, lekcje, które prowadzi, oraz opinie studentów. Rozpocznij naukę programowania pod okiem profesjonalisty!`,
+    [
+      "profil instruktora",
+      "instruktor programowania",
+      "loop instruktor",
+      "nauczyciel programowania",
+      "opinie o instruktorze",
+      "kursy prowadzone przez instruktora",
+      "doświadczenie instruktora",
+      "specjalizacje instruktora",
+      "mentor programistyczny",
+      "szkoła programowania",
+    ],
+  );
+
+  return {
+    title: metadata.title,
+    description: metadata.description,
+    keywords: metadata.keywords,
+  };
 }
