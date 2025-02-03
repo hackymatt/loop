04e6b0cfdf39e8bb81b7d1a720449d5c4dd84b38
loopedupl <admin@loop.edu.pl>
2025-02-03 23:18:09 +0100

    Add tests

--- backend/tests/test_purchase.py
+++ backend/tests/test_purchase.py
@@ -7,6 +7,7 @@ from .factory import (
     create_profile,
     create_student_profile,
     create_lecturer_profile,
+    create_admin_profile,
     create_course,
     create_lesson,
     create_technology,
@@ -46,6 +47,21 @@ class PurchaseTest(TestCase):
     def setUp(self):
         self.endpoint = "/api/purchase"
         self.client = APIClient()
+        self.admin_data = {
+            "email": "admin_test_email@example.com",
+            "password": "TestPassword123",
+        }
+        self.admin_user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.admin_data["email"],
+            password=self.admin_data["password"],
+            is_active=True,
+            is_staff=True,
+        )
+        self.admin_profile = create_admin_profile(
+            profile=create_profile(user=self.admin_user, user_type="A")
+        )
         self.data = {
             "email": "user@example.com",
             "password": "TestPassword123",
@@ -57,7 +73,17 @@ class PurchaseTest(TestCase):
             password=self.data["password"],
             is_active=True,
         )
+        self.user_2 = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email="user2@example.com",
+            password="TestPassword123",
+            is_active=True,
+        )
         self.profile = create_student_profile(profile=create_profile(user=self.user))
+        self.profile_2 = create_student_profile(
+            profile=create_profile(user=self.user_2)
+        )
 
         self.lecturer_user = create_user(
             first_name="first_name",
@@ -167,7 +193,7 @@ class PurchaseTest(TestCase):
             self.schedules.append(schedule)
         self.purchase_1 = create_purchase(
             lesson=self.lesson_1,
-            student=self.profile,
+            student=self.profile_2,
             price=self.lesson_1.price,
             payment=create_payment(amount=self.lesson_1.price),
         )
@@ -320,6 +346,17 @@ class PurchaseTest(TestCase):
     def test_get_purchase_authenticated(self):
         # login
         login(self, self.data["email"], self.data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        records_count = data["records_count"]
+        self.assertEqual(records_count, 5)
+
+    def test_get_purchase_authenticated_admin(self):
+        # login
+        login(self, self.admin_data["email"], self.admin_data["password"])
         self.assertTrue(auth.get_user(self.client).is_authenticated)
         # get data
         response = self.client.get(self.endpoint)
@@ -637,10 +674,14 @@ class PaymentStatusTest(TestCase):
         self.assertEqual(
             data,
             {
-                "amount": self.purchase_1.payment.amount,
+                "amount": int(self.purchase_1.payment.amount),
                 "order_id": self.purchase_1.payment.order_id,
                 "session_id": str(self.purchase_1.payment.session_id),
                 "status": self.purchase_1.payment.status,
+                "created_at": str(self.purchase_1.payment.created_at).replace(" ", "T")[
+                    0:26
+                ]
+                + "Z",
             },
         )
         self.assertEqual(notifications_number(), 1)
@@ -664,10 +705,14 @@ class PaymentStatusTest(TestCase):
         self.assertEqual(
             data,
             {
-                "amount": self.purchase_1.payment.amount,
+                "amount": int(self.purchase_1.payment.amount),
                 "order_id": self.purchase_1.payment.order_id,
                 "session_id": str(self.purchase_1.payment.session_id),
                 "status": "F",
+                "created_at": str(self.purchase_1.payment.created_at).replace(" ", "T")[
+                    0:26
+                ]
+                + "Z",
             },
         )
         self.assertEqual(notifications_number(), 1)
@@ -1399,6 +1444,273 @@ class PurchaseOrderTest(APITestCase):
                 ]
             else:
                 field_values = [course[field] for course in results]
+            if isinstance(field_values[0], dict):
+                self.assertEqual(
+                    field_values,
+                    sorted(field_values, key=lambda d: d["name"], reverse=True),
+                )
+            else:
+                field_values = [
+                    field_value if not is_float(field_value) else float(field_value)
+                    for field_value in field_values
+                ]
+                self.assertEqual(field_values, sorted(field_values, reverse=True))
+
+
+class PaymentTest(TestCase):
+    def setUp(self):
+        self.endpoint = "/api/payments"
+        self.admin_data = {
+            "email": "admin_test_email@example.com",
+            "password": "TestPassword123",
+        }
+        self.admin_user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.admin_data["email"],
+            password=self.admin_data["password"],
+            is_active=True,
+            is_staff=True,
+        )
+        self.admin_profile = create_admin_profile(
+            profile=create_profile(user=self.admin_user, user_type="A")
+        )
+        self.data = {
+            "email": "user@example.com",
+            "password": "TestPassword123",
+        }
+        self.user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.data["email"],
+            password=self.data["password"],
+            is_active=True,
+        )
+        self.profile = create_student_profile(profile=create_profile(user=self.user))
+
+        self.payment_1 = create_payment(amount=100)
+        self.payment_2 = create_payment(amount=50, status="P")
+        self.payment_3 = create_payment(amount=555, status="P")
+        self.payment_4 = create_payment(amount=22, status="F")
+        self.payment_5 = create_payment(amount=44)
+
+    def test_get_payments_unauthenticated(self):
+        # no login
+        self.assertFalse(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_payments_authenticated(self):
+        # login
+        login(self, self.data["email"], self.data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_get_payments_authenticated_admin(self):
+        # login
+        login(self, self.admin_data["email"], self.admin_data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        response = self.client.get(self.endpoint)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        records_count = data["records_count"]
+        self.assertEqual(records_count, 5)
+
+
+class PaymentFilterTest(APITestCase):
+    def setUp(self):
+        self.endpoint = "/api/payments"
+        self.admin_data = {
+            "email": "admin_test_email@example.com",
+            "password": "TestPassword123",
+        }
+        self.admin_user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.admin_data["email"],
+            password=self.admin_data["password"],
+            is_active=True,
+            is_staff=True,
+        )
+        self.admin_profile = create_admin_profile(
+            profile=create_profile(user=self.admin_user, user_type="A")
+        )
+        self.data = {
+            "email": "user@example.com",
+            "password": "TestPassword123",
+        }
+        self.user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.data["email"],
+            password=self.data["password"],
+            is_active=True,
+        )
+        self.profile = create_student_profile(profile=create_profile(user=self.user))
+
+        self.payment_1 = create_payment(amount=100)
+        self.payment_2 = create_payment(amount=50, status="P")
+        self.payment_3 = create_payment(amount=555, status="P")
+        self.payment_4 = create_payment(amount=22, status="F")
+        self.payment_5 = create_payment(amount=44)
+
+    def test_session_id_filter(self):
+        login(self, self.admin_data["email"], self.admin_data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        session_id = str(self.payment_1.session_id)
+        response = self.client.get(f"{self.endpoint}?session_id={session_id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        records_count = data["records_count"]
+        results = data["results"]
+        self.assertEqual(records_count, 1)
+        titles = list(set([session_id in record["session_id"] for record in results]))
+        self.assertTrue(len(titles) == 1)
+        self.assertTrue(titles[0])
+
+    def test_amount_from_filter(self):
+        login(self, self.admin_data["email"], self.admin_data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        amount_from = 49
+        response = self.client.get(f"{self.endpoint}?amount_from={amount_from}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        records_count = data["records_count"]
+        results = data["results"]
+        self.assertEqual(records_count, 3)
+        titles = list(
+            set([record["amount"] >= amount_from * 100 for record in results])
+        )
+        self.assertTrue(len(titles) == 1)
+        self.assertTrue(titles[0])
+
+    def test_amount_to_filter(self):
+        login(self, self.admin_data["email"], self.admin_data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        amount_to = 99
+        response = self.client.get(f"{self.endpoint}?amount_to={amount_to}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        records_count = data["records_count"]
+        results = data["results"]
+        self.assertEqual(records_count, 3)
+        titles = list(set([record["amount"] <= amount_to * 100 for record in results]))
+        self.assertTrue(len(titles) == 1)
+        self.assertTrue(titles[0])
+
+    def test_status_filter(self):
+        login(self, self.admin_data["email"], self.admin_data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        status_var = self.payment_4.status
+        response = self.client.get(f"{self.endpoint}?status={status_var}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        records_count = data["records_count"]
+        results = data["results"]
+        self.assertEqual(records_count, 1)
+        titles = list(set([status_var in record["status"] for record in results]))
+        self.assertTrue(len(titles) == 1)
+        self.assertTrue(titles[0])
+
+    def test_created_at_filter(self):
+        login(self, self.admin_data["email"], self.admin_data["password"])
+        self.assertTrue(auth.get_user(self.client).is_authenticated)
+        # get data
+        date = str(self.payment_1.created_at)[0:10]
+        response = self.client.get(f"{self.endpoint}?created_at={date}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = json.loads(response.content)
+        records_count = data["records_count"]
+        results = data["results"]
+        self.assertEqual(records_count, 5)
+        dates = list(set([date in record["created_at"] for record in results]))
+        self.assertTrue(len(dates) == 1)
+        self.assertTrue(dates[0])
+
+
+class PurchaseOrderTest(APITestCase):
+    def setUp(self):
+        self.endpoint = "/api/payments"
+        self.admin_data = {
+            "email": "admin_test_email@example.com",
+            "password": "TestPassword123",
+        }
+        self.admin_user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.admin_data["email"],
+            password=self.admin_data["password"],
+            is_active=True,
+            is_staff=True,
+        )
+        self.admin_profile = create_admin_profile(
+            profile=create_profile(user=self.admin_user, user_type="A")
+        )
+        self.data = {
+            "email": "user@example.com",
+            "password": "TestPassword123",
+        }
+        self.user = create_user(
+            first_name="first_name",
+            last_name="last_name",
+            email=self.data["email"],
+            password=self.data["password"],
+            is_active=True,
+        )
+        self.profile = create_student_profile(profile=create_profile(user=self.user))
+
+        self.payment_1 = create_payment(amount=100)
+        self.payment_2 = create_payment(amount=50, status="P")
+        self.payment_3 = create_payment(amount=555, status="P")
+        self.payment_4 = create_payment(amount=22, status="F")
+        self.payment_5 = create_payment(amount=44)
+
+        self.fields = [
+            "session_id",
+            "amount",
+            "status",
+            "created_at",
+        ]
+
+    def test_ordering(self):
+        for field in self.fields:
+            login(self, self.admin_data["email"], self.admin_data["password"])
+            self.assertTrue(auth.get_user(self.client).is_authenticated)
+            # get data
+            response = self.client.get(f"{self.endpoint}?sort_by={field}")
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+            data = json.loads(response.content)
+            count = data["records_count"]
+            results = data["results"]
+            self.assertEqual(count, 5)
+            field_values = [course[field] for course in results]
+            if isinstance(field_values[0], dict):
+                self.assertEqual(
+                    field_values, sorted(field_values, key=lambda d: d["name"])
+                )
+            else:
+                field_values = [
+                    field_value if not is_float(field_value) else float(field_value)
+                    for field_value in field_values
+                ]
+                self.assertEqual(field_values, sorted(field_values))
+            # get data
+            response = self.client.get(f"{self.endpoint}?sort_by=-{field}")
+            self.assertEqual(response.status_code, status.HTTP_200_OK)
+            data = json.loads(response.content)
+            count = data["records_count"]
+            results = data["results"]
+            self.assertEqual(count, 5)
+            field_values = [course[field] for course in results]
             if isinstance(field_values[0], dict):
                 self.assertEqual(
                     field_values,
